abc875715004694e1c99b49a254eea0f31702b8a
Sai Nimmagadda <snimmagadda@rlsolutions.com>
2016-06-24 11:15:16 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,5 @@ hiera_include('classes')
 node default{
  class { 'helloworld': }
  class { 'helloworld::motd': }
-## class { 'hieratest': }
- class('::timezone::tzname':)
+ class { 'hieratest': }
 }
